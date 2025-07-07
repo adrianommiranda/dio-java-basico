3b2f1210d6390e616d67559ef1a542957361e5bb
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2025-07-07 18:19:37 -0300

    Commit-Tipos-Variaveis-Final

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -5,5 +5,10 @@ public class TiposVariaveis {
 
         //cast
         short n = (short) normal;
+
+        System.out.println("O valor de n é = " + n);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println("valor constante de PI = " + VALOR_DE_PI);
     }
 }
