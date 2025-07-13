de7ad74a61d412d116812a81d96ca8fea2a98ef3
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2025-07-13 14:16:40 -0300

    Pare-Continue

---
+++ controle-fluxo/src/PareContinue.java
@@ -0,0 +1,12 @@
+public class PareContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if(i == 10){
+                continue;
+            }if(i == 15){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
